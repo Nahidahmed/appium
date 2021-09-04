c436621f33b3aa9f1b457355e1f587c7fa819bcd
Nahidahmed <nahid.mansuri@gmail.com>
2021-09-04 23:36:17 +0530

    Final version for sample ecommerce automation scripts

--- src/ecommerce_tc_4.java
+++ src/ecommerce_tc_4.java
@@ -4,17 +4,21 @@ import static io.appium.java_client.touch.offset.ElementOption.element;
 import static java.time.Duration.ofSeconds;
 
 import java.net.MalformedURLException;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
 
-public class ecommerce_tc_3 extends base {
+public class ecommerce_tc_4 extends base {
 
 	public static void main(String[] args) throws MalformedURLException, InterruptedException {
 		AndroidDriver<AndroidElement> driver = capabilities();
@@ -38,45 +42,25 @@ public class ecommerce_tc_3 extends base {
 		
 		Thread.sleep(2000);
 		
-		double totalVal = 0;
-		int count = driver.findElements(By.id("com.androidsample.generalstore:id/productPrice")).size();
-		for(int i=0;i<count;i++) {
-			String amt1 = driver.findElements(By.id("com.androidsample.generalstore:id/productPrice")).get(i).getText();
-			double amt1Val = getAmount(amt1);
-			totalVal += amt1Val;
-		}
-		
-		//The below code is optimized with above for loop
-//		String amt1 = driver.findElements(By.id("com.androidsample.generalstore:id/productPrice")).get(0).getText();
-//		double amt1Val = getAmount(amt1);
-//		
-//		String amt2 = driver.findElements(By.id("com.androidsample.generalstore:id/productPrice")).get(1).getText();
-//		double amt2Val = getAmount(amt2);
-//		
-//		double totalVal = amt1Val + amt2Val;
-		
-		String totalCartValStr = driver.findElement(By.id("com.androidsample.generalstore:id/totalAmountLbl")).getText();
-		double totalCartVal = getAmount(totalCartValStr);
-		
-		System.out.println("Calculated Value: "+ totalVal);
-		System.out.println("Cart Toal Value: "+ totalVal);
-		Assert.assertEquals(totalVal, totalCartVal);
 		
 		WebElement chkBox = driver.findElement(By.className("android.widget.CheckBox"));
 		
 		TouchAction t = new TouchAction(driver);
 		t.tap(tapOptions().withElement(element(chkBox))).perform();
 		
-		WebElement tc = driver.findElement(By.xpath("//*[@text='Please read our terms of conditions']"));
-		t.longPress(longPressOptions().withElement(element(tc)).withDuration(ofSeconds(2))).release().perform();
-		driver.findElement(By.id("android:id/button1")).click();
 		driver.findElement(By.id("com.androidsample.generalstore:id/btnProceed")).click();
 		
+		Thread.sleep(5000);
+		Set<String> contextNames = driver.getContextHandles();
+		for (String contextName : contextNames) {
+		    System.out.println(contextName); //prints out something like NATIVE_APP \n WEBVIEW_1
+		}
+		driver.context((String)contextNames.toArray()[1]); // set context to WEBVIEW_1
+		driver.findElement(By.name("q")).sendKeys("Nahid Ahmed Mansuri");
+		driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+		Thread.sleep(2000);
+		driver.pressKey(new KeyEvent(AndroidKey.BACK));
+
 	}
 
-	public static double getAmount(String amt) {
-		amt = amt.substring(1);
-		double updatedAmtVal = Double.parseDouble(amt);
-		return updatedAmtVal;
-	}
 }
